35c6f5526cb2a6c974c20b83fd79f06270cd3d7a
Keith Lee <keith.lee@flashcharity.org>
2019-11-19 23:53:09 +1100

    Update error pages

--- app/web/errors.py
+++ app/web/errors.py
@@ -5,14 +5,14 @@ from . import web
 
 @web.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', error=e), 404
 
 
 @web.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', error=e), 500
 
 
 @web.app_errorhandler(403)
 def forbidden(e):
-    return render_template('403.html'), 403
+    return render_template('403.html', error=e), 403
