f000e065292c4e07ff8add8c3b5e0903d349f774
mykangkr <miyae.kang@gmail.com>
2020-03-30 16:43:53 +1100

    Add: sql for username change to include CS Student ID

--- app/deploy/sql/new_username.sql
+++ app/deploy/sql/new_username.sql
@@ -3,18 +3,18 @@ select u.id,
 	s.student_id
 from users u, student s
 where u.id = s.user_id
-and u.username not like s.student_id ;
+and (username like '%(%' and username like '%)%');
 
 update users
 set username = ( select concat(u.username,' (',s.student_id,')') as new_username
                		from users u, student s
 					where u.id = s.user_id
 					and u.id = users.id)
-where username not like '(' and username not like ')';
+where not (username like '%(%' and username like '%)');
 
 select u.id,
 	u.username,
 	s.student_id
 from users u, student s
-where u.id = s.user_id
-and u.username not like s.student_id ;
+where u.id = s.user_id ;
+and (username like '%(%' and username like '%)%');
