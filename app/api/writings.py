e3d67943db8ee767fe8415f25e051d02df326e96
Keith Lee <keith.lee@flashcharity.org>
2020-04-13 13:35:44 +1000

    add logging

--- app/api/writings.py
+++ app/api/writings.py
@@ -4,24 +4,32 @@ from flask import request
 from app.api import api
 from app.decorators import permission_required_or_multiple
 from app.models import Item, Permission, AssessmentEnroll, MarkingForWriting, Codebook
+from common.logger import log
 
 
 # Writing > Assessment List > search writings > Items return for listing
+
 @api.route('/writing_item_list/')
 @permission_required_or_multiple(Permission.WRITING_READ, Permission.WRITING_MANAGE)
 def get_writing_item_list():
     assessment_guid = request.args.get('assessment_guid', '01', type=str)
     student_user_id = request.args.get('student_user_id', 0, type=int)
 
+    log.info("Get writing list for %s:%s" % (assessment_guid, student_user_id))
+
     marking_writing_list = []
     assessment_enroll = AssessmentEnroll.query.filter_by(assessment_guid=assessment_guid).filter_by(
         student_user_id=student_user_id).all()
+    log.debug("Assessment Enrolls: %s" % assessment_enroll)
     for a in assessment_enroll:
+        log.debug("Assessment: %s" % a)
         markings = a.marking
+        log.debug(markings)
         for m in markings:
             marking_writing = MarkingForWriting.query.filter_by(marking_id=m.id).first()
             if marking_writing:
                 item_subject_code = Item.query.filter_by(id=m.item_id).first().subject
+                log.debug(m)
                 if Codebook.get_code_name(item_subject_code) == Codebook.get_code_id("Writing"):
                     if marking_writing.candidate_file_link:
                         is_candidate_file = True
