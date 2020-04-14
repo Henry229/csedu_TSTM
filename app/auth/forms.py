766473fcbe3cc7770e85aea400b0815a384f7bcb
mykangkr <miyae.kang@gmail.com>
2020-04-14 14:35:20 +1000

    Update: Login Error Message

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -15,7 +15,8 @@ def get_roles():
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email(message='Type a correct email address.')])
+    email = StringField('Email',
+                        validators=[DataRequired(), Length(1, 64), Email(message='Type a correct email address.')])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
@@ -36,9 +37,10 @@ class RegistrationForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.branch.choices = [(u.id, u.code_name)
-                                for u in
-                                db.session.query(Codebook.id, Codebook.code_name).\
-                                   filter(Codebook.code_type == 'test_center').distinct().order_by(Codebook.code_name).all()]
+                               for u in
+                               db.session.query(Codebook.id, Codebook.code_name). \
+                                   filter(Codebook.code_type == 'test_center').distinct().order_by(
+                                   Codebook.code_name).all()]
 
     def validate_u_email(self, field):
         if User.query.filter_by(email=field.data).first():
@@ -76,6 +78,7 @@ class EditProfileAdminForm(FlaskForm):
     confirmed = BooleanField('Confirmed')
     role = SelectField('Role', id='u_role', coerce=int)
     branch = SelectMultipleField('Assign Branch', id='branch_ids', coerce=int)
+
     # name = StringField('Real name', validators=[Length(1,64)])
     # location = StringField('Location', validators=[Length(0,64)])
     # about_me = TextAreaField('About me')
@@ -85,8 +88,9 @@ class EditProfileAdminForm(FlaskForm):
         self.role.choices = [(role.id, role.name)
                              for role in Role.query.order_by(Role.name).all()]
         self.branch.choices = [(u.id, u.code_name)
-                                for u in
-                                db.session.query(Codebook.id, Codebook.code_name).filter(Codebook.code_type == 'test_center').\
+                               for u in
+                               db.session.query(Codebook.id, Codebook.code_name).filter(
+                                   Codebook.code_type == 'test_center'). \
                                    distinct().order_by(Codebook.code_name).all()]
         self.user = user
 
