09ce557b0a2db18092ff818ca261e63588dc16a5
Jennifer <jennifer.chung@cseducation.com.au>
2020-11-06 14:05:33 +1100

    fix bug interaction_type 6/11/2020 2:05pm

--- app/assessment/views.py
+++ app/assessment/views.py
@@ -561,6 +561,8 @@ def virtual_omr_sync(assessment_id=None, duration=3):
                     else:
                         # student answer is expected to be A, B, C, D which needs to be converted to 1, 2, 3, 4
                         try:
+
+                            vomr_logger.error("m.item_id: %s" % m.item_id)
                             item = Item.query.filter_by(id=m.item_id).first()
                             if item.interaction_type == "choiceInteraction":
                                 answers[str(m.question_no)] = ord(m.candidate_r_value) - 64
