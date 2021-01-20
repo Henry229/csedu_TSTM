d9bdf52707c5d89677ac3619b48b1dee300cce40
Jennifer <jennifer.chung@cseducation.com.au>
2021-01-21 09:21:16 +1100

    ENABLE_STUDENT_REPORT

--- config.py
+++ config.py
@@ -89,7 +89,7 @@ class Config(metaclass=MetaFlaskEnv):
                         'VIC': 'Branches in VIC'}
 
     # Student Reports
-    ENABLE_STUDENT_REPORT = False
+    ENABLE_STUDENT_REPORT = True
 
 
 class DevelopmentConfig(Config):
