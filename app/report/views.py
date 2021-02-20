5a99e8b1168c1482c605dd68598173f80765872a
Jennifer <jennifer.chung@cseducation.com.au>
2021-02-20 11:06:18 +1100

    enable_report testing.. fix bug testing..

--- app/report/views.py
+++ app/report/views.py
@@ -132,9 +132,7 @@ def my_report(assessment_id, ts_id, student_user_id):
     testset = Testset.query.with_entities(Testset.subject, Testset.grade, Testset.test_type).filter_by(id=row.testset_id).first()
     test_subject_string = Codebook.get_code_name(testset.subject)
     grade = Codebook.get_code_name(testset.grade)
-    test_type = Codebook.get_code_id(testset.test_type)
-    flash(testset.test_type)
-    flash(test_type)
+    test_type = testset.test_type
     # My Report : Header - 'total_students', 'student_rank', 'score', 'total_score', 'percentile_score'
 
     ts_header = query_my_report_header(assessment_enroll_id, assessment_id, ts_id, student_user_id)
@@ -174,7 +172,7 @@ def my_report(assessment_id, ts_id, student_user_id):
                                             score=score, markings=markings, ts_by_category=ts_by_category,
                                             student_user_id=student_user_id, static_folder=current_app.static_folder,
                                             pdf_url=pdf_url, grade=grade,
-                                            explanation_link=explanation_link, test_type=391)
+                                            explanation_link=explanation_link, test_type=test_type)
     if not pdf:
         return rendered_template_pdf
     # PDF download
