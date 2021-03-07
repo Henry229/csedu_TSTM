94aa1503b6058849a96b1c825711e0b39f06bcac
Jennifer <jennifer.chung@cseducation.com.au>
2021-03-08 00:20:06 +1100

    my report v2 - for testing

--- app/report/views.py
+++ app/report/views.py
@@ -202,6 +202,109 @@ def my_report(assessment_id, ts_id, student_user_id):
     return rsp
 
 
+@report.route('/ts_v2/<int:assessment_id>/<int:ts_id>/<student_user_id>', methods=['GET'])
+@login_required
+# @permission_required(Permission.ITEM_EXEC)
+@permission_required_or_multiple(Permission.ITEM_EXEC, Permission.ASSESSMENT_READ)
+def my_report_v2(assessment_id, ts_id, student_user_id):
+    '''
+     @report.route('/ts_v2/<int:assessment_id>/<int:ts_id>/<int:student_user_id>', methods=['GET'])
+     my_report() : Student Login > My Report > Report
+        - Execute: Provide link to Subject Report
+    '''
+    # Todo: Check accessibility to get report
+    # refresh_mviews()
+
+    pdf = False
+    pdf_url = "%s?type=pdf" % request.url
+    if 'type' in request.args.keys():
+        pdf = request.args['type'] == 'pdf'
+
+    query = AssessmentEnroll.query.with_entities(AssessmentEnroll.id, AssessmentEnroll.testset_id). \
+        filter_by(assessment_id=assessment_id). \
+        filter_by(testset_id=ts_id). \
+        filter_by(student_user_id=student_user_id)
+    row = query.order_by(AssessmentEnroll.id.desc()).first()
+    if row is None:
+        url = request.referrer
+        flash('Assessment Enroll data not available')
+        return redirect(url)
+
+    assessment_enroll_id = row.id
+    assessment_name = (Assessment.query.with_entities(Assessment.name).filter_by(id=assessment_id).first()).name
+    testset = Testset.query.with_entities(Testset.subject, Testset.grade, Testset.test_type).filter_by(id=row.testset_id).first()
+    test_subject_string = Codebook.get_code_name(testset.subject)
+    grade = Codebook.get_code_name(testset.grade)
+    test_type = testset.test_type
+    # My Report : Header - 'total_students', 'student_rank', 'score', 'total_score', 'percentile_score'
+
+    ts_header = query_my_report_header(assessment_enroll_id, assessment_id, ts_id, student_user_id)
+    if ts_header is None:
+        url = request.referrer
+        flash('Marking data not available')
+        return redirect(url)
+    score = '{} out of {} ({}%)'.format(ts_header.score, ts_header.total_score, ts_header.percentile_score)
+    rank = '{} out of {}'.format(ts_header.student_rank, ts_header.total_students)
+    # My Report : Body - Item ID/Candidate Value/IsCorrect/Correct_Value, Correct_percentile, Item Category
+    #                       'assessment_enroll_id', 'testset_id', 'candidate_r_value', 'student_user_id', 'grade',
+    #                       "created_time", 'is_correct', 'correct_r_value', 'item_percentile', 'item_id', 'category'
+    markings = query_my_report_body(assessment_enroll_id, ts_id)
+    explanation_link = {}
+    for marking in markings:
+        explanation_link[marking.question_no] = view_explanation(testset_id=ts_id, item_id=marking.item_id)
+
+    # My Report : Footer - Candidate Avg Score / Total Avg Score by Item Category
+    #                       'code_name as category', 'score', 'total_score', 'avg_score', 'percentile_score'
+    # ToDo: ts_by_category unavailable until finalise all student's mark and calculate average data
+    #       so it need to be discussed to branch out in "test analysed report"
+    # ts_by_category = None
+    ts_by_category = query_my_report_footer(assessment_id, student_user_id)
+
+    if test_subject_string == 'Writing':
+        marking_writing_id = 0
+        url_i = url_for('writing.w_report', assessment_enroll_id=assessment_enroll_id,
+                        student_user_id=student_user_id, marking_writing_id=marking_writing_id)
+        return redirect(url_i)
+
+    template_file = 'report/my_report.html'
+    if pdf:
+        template_file = 'report/my_report_pdf.html',
+
+    rendered_template_pdf = render_template(template_file, assessment_name=assessment_name,
+                                            subject=test_subject_string, rank=rank,
+                                            score=score, markings=markings, ts_by_category=ts_by_category,
+                                            student_user_id=student_user_id, static_folder=current_app.static_folder,
+                                            pdf_url=pdf_url, grade=grade,
+                                            explanation_link=explanation_link, test_type=test_type)
+    if not pdf:
+        return rendered_template_pdf
+    # PDF download
+    from weasyprint import HTML
+
+    html = HTML(string=rendered_template_pdf)
+
+    pdf_file_path = os.path.join(current_app.config['USER_DATA_FOLDER'],
+                                 str(student_user_id),
+                                 "report",
+                                 "test_report_%s_%s_%s_%s.pdf" % (
+                                     assessment_enroll_id, assessment_id, ts_id, student_user_id))
+
+    os.chdir(os.path.join(current_app.config['USER_DATA_FOLDER']))
+    if not os.path.exists(str(student_user_id)):
+        os.makedirs(str(student_user_id))
+    os.chdir(str(student_user_id))
+    if not os.path.exists("report"):
+        os.makedirs("report")
+
+    html.write_pdf(target=pdf_file_path, presentational_hints=True)
+    rsp = send_file(
+        pdf_file_path,
+        mimetype='application/pdf',
+        as_attachment=True,
+        attachment_filename=pdf_file_path)
+    return rsp
+
+
 @report.route('/student/set/<int:assessment_id>/<student_user_id>', methods=['GET'])
 @login_required
 @permission_required(Permission.ITEM_EXEC)
