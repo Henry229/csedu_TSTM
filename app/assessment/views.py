c9702e49329fe518c1a8ebcd3391c3f2f2314db9
Jennifer <jennifer.chung@cseducation.com.au>
2021-04-18 21:28:43 +1000

    do sync when test_type in cbstt', 'cboctt', 'oncboctt', 'oncbstt'

--- app/assessment/views.py
+++ app/assessment/views.py
@@ -500,7 +500,8 @@ def virtual_omr_sync(assessment_id=None, duration=3):
 
             for enroll in enrolls:
                 # pass to sync for homework
-                if test_type_name.lower().find('homework') >= 0:
+                # if test_type_name.lower().find('homework') print>= 0:
+                if test_type_name.lower() not in ['cbstt', 'cboctt', 'oncboctt', 'oncbstt']:
                     continue
                 testset = Testset.query.filter_by(id=enroll.testset_id).first()
                 subject = Codebook.get_subject_name(testset.id)
