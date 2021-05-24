c6aa5da41a022161a66a88bc20cfbd244d1e0a13
tailored_cseducation <itsupport@cseducation.com.au>
2021-05-24 20:25:43 +1000

    writing saving when timeout

--- app/api/omr.py
+++ app/api/omr.py
@@ -3,17 +3,24 @@ import json
 from app.api import api
 from flask import jsonify, request
 
+from app.api.errors import bad_request
+from config import Config
+
 
 @api.route('/omr/marking', methods=['POST'])
-#@login_required
 def omr_marking():
+    if request.headers['Authorization'] == None or request.headers['Authorization'] != Config.SYNC_SECRET_KEY:
+        return bad_request()
+
     retsult = json.dumps(request.json.get('result'))
 
     return jsonify(retsult)
 
 @api.route('/omr/writing', methods=['POST'])
-#@login_required
 def omr_writing():
+    if request.headers['Authorization'] == None or request.headers['Authorization'] != Config.SYNC_SECRET_KEY:
+        return bad_request()
+
     retsult = json.dumps(request.json.get('result'))
 
     return jsonify(retsult)
