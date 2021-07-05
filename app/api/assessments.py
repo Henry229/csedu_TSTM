b7dd37311c4709e5daba9678fd502cc94d5fcc1d
tailored_cseducation <itsupport@cseducation.com.au>
2021-07-05 12:35:27 +1000

    checking back button

--- app/api/assessments.py
+++ app/api/assessments.py
@@ -593,13 +593,15 @@ def response_process(item_id, assessment_session=None):
             data['html'] = render_template("runner/stage_finished.html", start=start, end=end)
             data['testlet_id'] = marking_testlet_id
             data['question_no'] = question_no
-            assessment_session.set_status(AssessmentSession.STATUS_STAGE_FINISHED)
+            if direction != 'back':
+                assessment_session.set_status(AssessmentSession.STATUS_STAGE_FINISHED)
         else:
             testset_id = assessment_session.get_value('testset_id')
             testset = Testset.query.with_entities(Testset.test_type).filter_by(id=testset_id).first()
             test_type = testset.test_type
             assessment_session.set_status(AssessmentSession.STATUS_TEST_FINISHED)
-            data['html'] = render_template("runner/test_finished.html", test_type=test_type)
+            if direction != 'back':
+                data['html'] = render_template("runner/test_finished.html", test_type=test_type)
     else:
         next_item_id = next_item.get('item_id')
         next_question_no = None
