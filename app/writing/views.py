50bee25a6a2fe61d2ebb2895cebb13190aa2efc9
tailored_cseducation <itsupport@cseducation.com.au>
2021-07-22 16:01:13 +1000

    file download

--- app/writing/views.py
+++ app/writing/views.py
@@ -119,6 +119,7 @@ def list_writing_marking_download(marking_writing_id, student_user_id):
 
     marking_writing = MarkingForWriting.query.filter_by(id=marking_writing_id).first()
     if marking_writing is not None:
+        os.chdir(current_app.config['USER_DATA_FOLDER'])
         zip_name = "writing_" + str(marking_writing_id) + "_" + Student.getCSStudentId(student_user_id)
         zfile = os.path.join(current_app.config['USER_DATA_FOLDER'],
                              str(student_user_id),
@@ -130,8 +131,8 @@ def list_writing_marking_download(marking_writing_id, student_user_id):
                 if file_name:
                     file_path = os.path.join(current_app.config['USER_DATA_FOLDER'], str(student_user_id), "writing",
                                              file_name)
-                    #if os.path.exists(file_path):
-                    #    zip.write(os.path.join(str(student_user_id), file_name))
+                    if os.path.exists(file_path):
+                        zip.write(os.path.join(str(student_user_id), file_name))
 
 
         rsp = send_file(
