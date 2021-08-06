5d62602b8a6b4cfe8dca2fa9180c0ca5ba4ce09a
tailored_cseducation <itsupport@cseducation.com.au>
2021-08-06 11:29:33 +1000

    writing saving when timeout

--- app/writing/views.py
+++ app/writing/views.py
@@ -453,7 +453,7 @@ def get_merged_images(student_user_id, marking_writing, local_file=False, vertic
                 c_image.save(
                     os.path.join(current_app.config['USER_DATA_FOLDER'],
                                  str(student_user_id),
-                                 "writing", v.replace('.tif', '.jpg')))
+                                 "writing", v.replace('.tif', '.jpg')), "PNG")
         except FileNotFoundError:
             log.error('File not found. Check the student writing file existing')
 
