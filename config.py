3a5a559592cf3e13a6cd736588aea50e2f60ea6e
tailored_cseducation <itsupport@cseducation.com.au>
2021-08-12 21:24:10 +1000

    writing saving when timeout

--- config.py
+++ config.py
@@ -14,13 +14,12 @@ class Config(metaclass=MetaFlaskEnv):
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     #MAIL_USERNAME = 'testsystem@cseducation.com.au'
     #MAIL_PASSWORD = 'tSm4$ewojas'
-    MAIL_USERNAME = 'hverityg@gmail.com'
+    #MAIL_USERNAME = 'hverityg@gmail.com'
     #MAIL_PASSWORD = 'dgqnkybpdmfsuesh'
-    MAIL_PASSWORD = 'ylzrjvkjgacycgcf'
     AUTHORIZATION_KEY = '!x7!Hn66S0-p2U3zZm8?sKe4V4==XQCz'
 
     CSEDU_MAIL_SUBJECT_PREFIX = '[CSEDU]'
