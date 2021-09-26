e002b2b7e3c15eac8cd4a6388102a2fbb34d4c3b
tailored_cseducation <itsupport@cseducation.com.au>
2021-09-26 23:30:30 +1000

    omr

--- app/api/omr.py
+++ app/api/omr.py
@@ -1,25 +1,168 @@
 import json
 import subprocess
 from collections import namedtuple
+from datetime import datetime, timedelta
+import re
 
 from app.api import api
 from flask import jsonify, request, current_app
 
 from app.api.errors import bad_request
-from app.models import AssessmentEnroll, Assessment, Codebook, Student, Marking, Item
+from app.models import AssessmentEnroll, Assessment, Codebook, Student, Marking, Item, Testset, AssessmentHasTestset, \
+    TestletHasItem
 from config import Config
 from qti.itemservice.itemservice import ItemService
 from .assessments import parse_processed_response, parse_correct_response, allowed_file, save_writing_data
-from .response import success
+from .response import success, fail
 from .. import db
 
 
 @api.route('/omr/marking', methods=['POST', 'GET'])
 def omr_marking():
-    #if request.headers['Authorization'] == None or request.headers['Authorization'] != Config.AUTHORIZATION_KEY:
-    #    return bad_request()
+    if request.headers['Authorization'] is None:
+        return bad_request(message="The Authorization is not correct.")
+    authorization = request.headers['Authorization'].split(' ')
+    if len(authorization) != 2:
+        return bad_request(message="The Authorization is not correct.")
+    else:
+        if authorization[0] != 'KEY' or authorization[1] != Config.AUTHORIZATION_KEY:
+            return bad_request(message="The Authorization is not correct.")
 
-    #retsult = request.json.get('result')
+    if len(request.json) !=3:
+        return bad_request(message="The parameter's length is not correct.")
+
+    scores_list = request.json[0]
+    examInfo = request.json[1]
+    info = request.json[2]
+
+    student = Student.query.filter_by(student_id=info[0].get("student_id")).first()
+    if student is None:
+        return bad_request(message="Student does not exist.")
+
+    test_center = Codebook.query.filter(Codebook.code_type == 'test_center',
+                                        Codebook.additional_info.contains(
+                                            {"campus_prefix": student.branch.strip()})).first()
+    test_center_id = None
+    if test_center:
+        test_center_id = test_center.id
+
+    for _info in info:
+        scores = list(filter(lambda x: (x["Subject"] == _info["subject"]), scores_list))
+
+        #testset = Testset.query.filter_by(GUID=_info.get('testset_guid'), active=True).first()
+        #if testset is None:
+        #    return bad_request(message="The testset does not exist.")
+
+        #_info['testset_guid'] = '3b1a9db6-3be2-4e67-b566-2a44f3675539'
+        _info['testset_guid'] = '8b5b575c-7219-48ea-98d4-8fead8ef55bf'
+
+        assessment = Assessment.query.with_entities(Assessment.id.label('assessment_id'), Assessment.test_type, Testset.test_duration,
+                          Assessment.GUID.label('assessment_guid'), Testset.id.label('testset_id'),
+                        Testset.branching).\
+                    join(AssessmentHasTestset, Assessment.id == AssessmentHasTestset.assessment_id).\
+                        join(Testset, AssessmentHasTestset.testset_id == Testset.id).\
+                        filter(Testset.GUID == _info.get('testset_guid'), Testset.active == True).first()
+        if assessment is None:
+            return bad_request(message="The assessment does not exist.")
+
+        '''
+        AssessmentEnroll
+        '''
+        assessment_guid = assessment.assessment_guid
+        student_user_id = student.user_id
+        attempt_count = 1
+        test_type_name = Codebook.get_code_name(assessment.test_type)
+        dt = datetime.utcnow()
+        start_time = dt + timedelta(minutes=(-1 * assessment.test_duration))
+        testset_id = assessment.testset_id
+
+        enrolled = AssessmentEnroll(assessment_guid=assessment_guid, assessment_id=assessment.assessment_id, testset_id=testset_id,
+                                    student_user_id=student_user_id, attempt_count=attempt_count,
+                                    start_time=start_time, finish_time=dt, assessment_type=test_type_name, test_center=test_center_id)
+        db.session.add(enrolled)
+        db.session.commit()
+        assessment_enroll_id = enrolled.id
+
+        '''
+        Marking
+        '''
+        item_list = []
+        branching = json.dumps(assessment.branching)
+        ends = [m.end() for m in re.finditer('"id":', branching)]
+        for end in ends:
+            comma = branching.find(',', end)
+            testlet_id = int(branching[end:comma])
+
+            items = db.session.query(*Item.__table__.columns,TestletHasItem.weight, TestletHasItem.order). \
+                select_from(Item). \
+                join(TestletHasItem, Item.id == TestletHasItem.item_id). \
+                filter(TestletHasItem.testlet_id == testlet_id).order_by(TestletHasItem.order).all()
+
+            for item in items:
+                i = {'item_id':item.id,
+                     'testlet_id':testlet_id,
+                     'order':item.order,
+                     'correct_r_value':item.correct_r_value,
+                     'weight':item.weight,
+                     'outcome_score':item.outcome_score
+                     }
+                item_list.append(i)
+
+        for item in item_list:
+            score = list(filter(lambda x: (x["QuestionNo"] == str(item.get('order'))), scores))
+
+            qti_item_obj = Item.query.filter_by(id=item.get('item_id')).first()
+            db.session.expunge(qti_item_obj)
+
+            processed = None
+            try:
+                item_service = ItemService(qti_item_obj.file_link)
+                qti_xml = item_service.get_qti_xml_path()
+                processing_php = current_app.config['QTI_RSP_PROCESSING_PHP']
+                response = {"RESPONSE": {
+                    "base": {
+                        "identifier": marking_to_value(score)
+                    }
+                }}
+                parameter = json.dumps({'response': response, 'qtiFilename': qti_xml})
+                try:
+                    result = subprocess.run(['php', processing_php, parameter], stdout=subprocess.PIPE)
+                    processed = result.stdout.decode("utf-8")
+                    processed = json.loads(processed)
+                except Exception as e:
+                    response['processed'] = "Not implemented."
+            except Exception as e:
+                print(e)
+            if processed is None:
+                return bad_request(message="Processing response error")
+
+            if response.get("RESPONSE") and response.get("RESPONSE").get("base") and response.get("RESPONSE").get(
+                    "base").get('file'):
+                candidate_response = response.get("RESPONSE").get("base")
+            else:
+                candidate_response = parse_processed_response(processed.get('RESPONSE'))
+
+            candidate_r_value = candidate_response
+            candidate_mark = processed.get('SCORE')
+            outcome_score = processed.get('maxScore')
+            is_correct = candidate_mark >= outcome_score
+            correct_r_value = parse_correct_response(processed.get('correctResponses'))
+
+            marking = Marking(testset_id=testset_id,
+                              testlet_id=testlet_id,
+                              item_id=item.get('item_id'),
+                              question_no=item.get('order'),
+                              weight=item.get('weight'),
+                              candidate_r_value=candidate_r_value,
+                              candidate_mark=candidate_mark,
+                              outcome_score=outcome_score,
+                              is_correct=is_correct,
+                              correct_r_value=correct_r_value,
+                              is_read=True,
+                              is_flagged=False,
+                              assessment_enroll_id=assessment_enroll_id)
+            db.session.add(marking)
+        db.session.commit()
 
     return success({"result": "success"})
 
@@ -67,3 +210,14 @@ def receive_image(f):
     handler.write(image_data)
     handler.close()
     return handler
+
+def marking_to_value(score):
+    alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    candidate_r_value = []
+    markings = score[0]["marking"]
+
+    for idx, val in enumerate(markings):
+        if val:
+            candidate_r_value.append(alfa[idx])
+
+    return candidate_r_value
