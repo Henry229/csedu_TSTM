55fd892572f57ae199701f74ae876619f2659b7f
tailored_cseducation <itsupport@cseducation.com.au>
2021-09-29 21:40:46 +1000

    homework

--- app/web/views.py
+++ app/web/views.py
@@ -690,8 +690,6 @@ def assessment_list():
                 if len(_subjects['list']) > homework['header_count']:
                     homework['header_count'] = len(_subjects['list'])
 
-        #my_set = set(homeworks_grouped)
-        #homeworks_grouped = list(homeworks_grouped)
 
     assessments_list = {"Class Test": class_assessments, "Trial Test": trial_assessments, "Homework": homeworks_grouped}
     log.debug("Student report: %s" % Config.ENABLE_STUDENT_REPORT)
@@ -735,7 +733,7 @@ def assessment_list():
         runner_version = str(int(datetime.utcnow().timestamp()))
     return render_template('web/assessments.html', student_user_id=current_user.id, assessments_list=assessments_list,
                            runner_version=runner_version, btn_all=btn_all, btn_class=btn_class, btn_trial=btn_trial,
-                           btn_homework=btn_homework, btn_group=btn_group, unit=homework_days, test=homeworks_grouped)
+                           btn_homework=btn_homework, btn_group=btn_group, unit=homework_days)
 
 
 @web.route('/tests/assessments/report', methods=['GET'])
