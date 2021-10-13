8ac0b43b8852b0c0e0bcd89c4d42b1c6bebb82a8
tailored_cseducation <itsupport@cseducation.com.au>
2021-10-13 17:10:03 +1100

    report in vocabulary

--- app/report/views.py
+++ app/report/views.py
@@ -346,7 +346,7 @@ def vocabulary_report(request, assessment_id, ts_id, student_user_id, testset, t
     pdf_file_path = os.path.join(current_app.config['USER_DATA_FOLDER'],
                                  str(student_user_id),
                                  "report",
-                                 "test_report_%s_%s_%s_%s_.pdf" % (
+                                 "testtest_report_%s_%s_%s_%s.pdf" % (
                                      assessment_enroll_id, assessment_id, ts_id, student_user_id))
 
     os.chdir(os.path.join(current_app.config['USER_DATA_FOLDER']))
