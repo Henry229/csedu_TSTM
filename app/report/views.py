749cf31279a9e65e79be06811ad3ca3c40e809ba
tailored_cseducation <itsupport@cseducation.com.au>
2021-10-13 20:05:06 +1100

    report in vocabulary

--- app/report/views.py
+++ app/report/views.py
@@ -322,7 +322,7 @@ def vocabulary_report(request, assessment_id, ts_id, student_user_id, testset, t
                      'id': row.id,
                      'is_correct': is_correct})
 
-    correct_count = len([l.id for l in list])
+    correct_count = len([item.id for item in list])
     #correct_count = len(list(filter(lambda x: (x.is_correct == True), list)))
     score = '{} out of {}'.format(correct_count, len(list))
 
