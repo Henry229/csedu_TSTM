bd9e9e7936269bbeed88d2fcf79aee2f662ef059
tailored_cseducation <itsupport@cseducation.com.au>
2021-10-20 15:59:38 +1100

    verbal

--- app/errornote/views.py
+++ app/errornote/views.py
@@ -103,6 +103,31 @@ def error_note(assessment_enroll_id):
                 marking.verbal_candidate_r_value = candidate_r_values
                 marking.candidate_all_correct = candidate_all_correct
 
+
+            last_r_values = []
+            for r_value in marking.correct_r_value:
+                is_existent = False
+                r_ques_no = '0'
+                if r_value.find(" gap_") > -1:
+                    r_ques_no = r_value[r_value.rfind('_') + 1:]
+                for value in marking.last_r_value:
+                    if value.find(" gap_") > -1:
+                        ques_no = value[value.rfind('_') + 1:]
+                        if r_ques_no == ques_no:
+                            _is_correct = False
+                            if r_value == value:
+                                _is_correct = True
+                            end = value.index(" gap_")
+                            ques_last_value = value[0:end]
+                            last_r_values.append({'no':str(len(last_r_values)+1), 'value':ques_last_value, 'correct': _is_correct})
+                            is_existent = True
+                if not is_existent:
+                    last_r_values.append({'no': str(len(last_r_values)+1), 'value': '', 'correct': False})
+
+            if len(last_r_values) > 0:
+                marking.verbal_last_r_value = last_r_values
+
+
         markings.append(marking)
     correct_percent = correct_count * 100.0 / question_count
     score = '{} out of {} ({:.2f}%)'.format(correct_count, question_count, correct_percent)
