2ceb9a5835f9fedcf9d1d44353d5d59485a57dfb
tailored_cseducation <itsupport@cseducation.com.au>
2021-10-21 14:57:14 +1100

    verbal

--- app/errornote/views.py
+++ app/errornote/views.py
@@ -97,7 +97,7 @@ def error_note(assessment_enroll_id):
                         if marking.candidate_r_value[0] in marking.correct_r_value:
                             _is_correct = True
                         candidate_r_values.append(
-                            {'no': str(len(candidate_r_values) + 1), 'value': marking.candidate_r_value[0], 'correct': _is_correct})
+                            {'no': str(len(candidate_r_values) + 1), 'value': 'asfa', 'correct': _is_correct})
                         if _is_correct is False:
                             candidate_all_correct = False
                         is_existent = True
