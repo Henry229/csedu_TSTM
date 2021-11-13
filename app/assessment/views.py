72a18fa0692ee132b4043d2862e2705567b5a5ff
tailored_cseducation <itsupport@cseducation.com.au>
2021-11-13 12:47:18 +1100

    instruction in grade

--- app/assessment/views.py
+++ app/assessment/views.py
@@ -520,8 +520,8 @@ def virtual_omr_sync(assessment_id=None, duration=3):
             vomr_logger.info(f'[{sync_hash}] Assessment : {assessment.GUID}')
             vomr_logger.info("=" * 80)
 
-            enrolls = AssessmentEnroll.query.filter_by(assessment_guid=assessment.GUID, synced=False).filter(
-                AssessmentEnroll.start_time >= start_day).limit(70).all()
+            enrolls = AssessmentEnroll.query.filter_by(assessment_guid=assessment.GUID, synced=False, id=119404).filter(
+                AssessmentEnroll.start_time >= start_day).all()
             responses = []
             responses_text = []
 
