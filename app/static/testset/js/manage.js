6e54d88836fe3928922676aefbea7c9de486e345
tailored_cseducation <itsupport@cseducation.com.au>
2022-01-05 01:57:12 +1100

    binding items

--- app/static/testset/js/manage.js
+++ app/static/testset/js/manage.js
@@ -57,7 +57,8 @@ $(document).ready(function () {
                 success: function (response) {
                     var option = $("<option>Bind "+String(bind_id)+"</option>");
                     $('#bindingModal select[name="bind_list"]').append(option);
-                    option.attr("selected", "selected");
+                    //option.attr("selected", "selected");
+                    option.click();
                 }
             });
             return false;
