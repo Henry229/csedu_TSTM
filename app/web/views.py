174760fd4c6aab7d0cb860ad911bc6c44cd5bb12
tailored_cseducation <itsupport@cseducation.com.au>
2022-03-18 13:32:47 +1100

    sa connection test

--- app/web/views.py
+++ app/web/views.py
@@ -205,13 +205,10 @@ def process_inward():
         }
     else:
         try:
-            log.debug("Hongseok state: %s" % state)
-            log.debug("Hongseok state: %s" % student_id)
             member = get_student_info(state, student_id)
         except:
             return forbidden("Invalid Request")
-        authorised = True
-        #authorised, errors = is_authorised(member, session_timeout)
+        authorised, errors = is_authorised(member, session_timeout)
     if authorised:
         # registered_student = Student.query.filter(Student.student_id.ilike(student_id), Student.state == state).first()
         # ilike can't find exact matching student id e.g. ethan_H
