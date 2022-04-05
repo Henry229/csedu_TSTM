7b125ae12752f6a5c5b0aba09e6444da770e8ca2
tailored_cseducation <itsupport@cseducation.com.au>
2022-04-05 14:44:52 +1000

    writing list

--- app/writing/views.py
+++ app/writing/views.py
@@ -124,7 +124,7 @@ def list_writing_marking():
 
     query = db.session.query(AssessmentEnroll, Marking, MarkingForWriting). \
         join(Marking, AssessmentEnroll.id == Marking.assessment_enroll_id). \
-        join(MarkingForWriting, Marking.id == MarkingForWriting.marking_id). \
+        outerjoin(MarkingForWriting, Marking.id == MarkingForWriting.marking_id). \
         filter(Marking.assessment_enroll_id.in_(assessment_enroll_ids))
     if tabs == '1':
         if marked == '1':
