f649ec94f4b4040b4d4d3ee078be18afae5194fd
tailored_cseducation <itsupport@cseducation.com.au>
2022-06-08 22:28:10 +1000

    inserted date in the list

--- app/api/reports.py
+++ app/api/reports.py
@@ -593,6 +593,9 @@ def query_individual_progress_summary_report_by_plan(plan_id, student_user_id):
 
 def query_report_graph(assessment_id, student_user_id):
     sql_stmt = 'select t.testset_id, t.id as assessment_enroll_id, t.score, t.ranking, t.total, ' \
+               '(select (select code_name from codebook where id = t2.subject) from testset t2 where id = t.testset_id) as subject,' \
+               'round((ranking / cast( total as DECIMAL(4,1))) * 100) as my_pecent ' \   
+               'from ( ' \
                'order by id'
     cursor = db.session.execute(text(sql_stmt), {'assessment_id': assessment_id, 'student_user_id': student_user_id})
     Record = namedtuple('Record', cursor.keys())
