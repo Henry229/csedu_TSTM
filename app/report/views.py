5c234bbc5faaf69873ff29ae54b77aadc17ac662
tailored_cseducation <itsupport@cseducation.com.au>
2022-12-18 22:50:39 +0900

    test

--- app/report/views.py
+++ app/report/views.py
@@ -831,9 +831,7 @@ def center():
         search_form.test_center.data = test_center
     search_form.year.data = year
 
-    log.info("chs1")
     if assessment_id == 0:
-        log.info("chs2")
         return render_template('report/report_center.html', form=search_form, report_list='', columns_list='')
 
     assessments = search_assessment()
