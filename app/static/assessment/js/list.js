0f519310a5dd815a0e4233139cd51d50373f7ab5
tailored_cseducation <itsupport@cseducation.com.au>
2023-03-17 11:41:10 +1100

    test

--- app/static/assessment/js/list.js
+++ app/static/assessment/js/list.js
@@ -57,34 +57,6 @@ function makeTestQeustions(o, id){
     var data = {
         id: id
     };
-    $.ajax({
-        method: 'GET',
-        url: '/api/testsets/questions/',
-        data: data,
-        beforeSend: function () {
-            $btn.attr('disabled', true);
-        },
-        success: function (data) {
-            var str = '';
-            $('#testsets_searched > tbody').empty();
-            if (data.length == 0) {
-                str = '<tr><td colspan="5">No data found.</td></tr>';
-                $('#testsets_searched > tbody').append(str);
-            } else {
-                $('#testsets_searched > tbody').append(str);
-                data.forEach(function (item) {
-                    str = '<tr><td><button class="btn btn-sm btn-outline-info" type="button" name="modalButton"' +
-                        ' data-toggle="modal" data-target="#dataModal" onclick=invokeModalTestset(' + item[0] + ')><i class="fas fa-eye"></i></button>';
-                    str = str + '<button class="btn btn-sm btn-outline-info" type="button" onclick=
-                    str = str + '</td>';
-                    str = str + '<td class="search_item" id="item_id">' + item[0] + '</td>';
-                    str = str + '<td class="search_item" id="item_name">' + item[1] + '</td>';
-                    str = str + '<td class="search_item" id="item_grade">' + item[2] + '</td>';
-                    str = str + '<td class="search_item" id="item_subject">' + item[3] + '</td></tr>';
-                    $('#testsets_searched > tbody').append(str);
-                });
-            }
-        }
-    });
+
 
 }
