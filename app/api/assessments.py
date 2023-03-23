45227aec882a2759e750a63f6934b77e1ce84048
tailored_cseducation <itsupport@cseducation.com.au>
2023-03-23 14:52:52 +1100

    test

--- app/api/assessments.py
+++ app/api/assessments.py
@@ -1382,7 +1382,6 @@ def _get_testsetsQues():
                     rendered_template = rendered_template.replace('rendered_html', rendered_item)
                 response['html'] = rendered_template
 
-                qti_item_obj.html = response
-        db.session.commit()
+
 
     return success()
