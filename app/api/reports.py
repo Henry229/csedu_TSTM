8b1039bc7cdc1213ee8aebe412a878d8dfffeba3
tailored_cseducation <itsupport@cseducation.com.au>
2023-05-20 10:25:20 +1000

    test

--- app/api/reports.py
+++ app/api/reports.py
@@ -299,6 +299,66 @@ def query_my_report_header(assessment_enroll_id, assessment_id, ts_id, student_u
                     ]
 
     # Combining the two tests
+    sql_stmt = "SELECT " \
+               "rank_v as student_rank, " \
+               "total_students, " \
+               "to_char(score,'999.99') as score, " \
+               "to_char(total_score,'999.99') as total_score, " \
+               "to_char(percentile_score,'999.99') as percentile_score, " \
+               "(select count(distinct bbb.student_user_id) " \
+               "from (select student_user_id, assessment_id, max(id) as id " \
+               "from assessment_enroll " \
+               "where assessment_id in (" \
+               "  select id from assessment where id in(select assessment_id from education_plan_details aaaa where plan_id = test_summary_mview.plan_id) " \
+               "                 and test_detail = (select test_detail from assessment where id =:assessment_id) " \
+               ") " \
+               "group by student_user_id, assessment_id " \
+               ") bbb " \
+               "where exists(select 1 from marking where assessment_enroll_id = bbb.id and student_user_id = bbb.student_user_id) " \
+               ") AS total_students1, " \
+    "( " \
+    "    select student_rank " \
+    "    from ( " \
+    "        select student_user_id, rank() OVER(ORDER by score desc) as student_rank " \
+    "        from ( " \
+    "            select aaa.student_user_id, " \
+    "                case when sum(bbb.outcome_score * bbb.weight) = 0 then 0 else " \
+    "                    sum(bbb.candidate_mark * bbb.weight) * 100::double precision / sum(bbb.outcome_score * bbb.weight) end as score " \
+    "            from (select student_user_id, assessment_id, max(id) as id from assessment_enroll " \
+    "                    where assessment_id in ( " \
+    "                        select id from assessment where id in (select assessment_id from education_plan_details aaaa where plan_id = test_summary_mview.plan_id) " \
+    "                            and test_detail = (select test_detail from assessment where id =:assessment_id) " \
+    "                           ) " \
+    "                   group by student_user_id, assessment_id " \
+    "                ) aaa " \
+    "            join marking bbb " \
+    "                on aaa.id = bbb.assessment_enroll_id " \
+    "            group by aaa.student_user_id " \
+    "        ) rnk " \
+    "    ) tt " \
+    "    where student_user_id = test_summary_mview.student_user_id " \
+    ") as student_rank1 " \
+"FROM test_summary_mview " \
+               "WHERE assessment_enroll_id=:assessment_enroll_id " \
+               "and assessment_id=:assessment_id and testset_id=:testset_id " \
+               "and student_user_id=:student_user_id"
+
+    cursor = db.session.execute(sql_stmt,
+                                {'assessment_enroll_id': assessment_enroll_id, 'assessment_id': assessment_id,
+                                 'testset_id': ts_id, 'student_user_id': student_user_id})
+    ts_header = cursor.fetchone()
+    return ts_header
+
+
+def query_my_report_header_1(assessment_enroll_id, assessment_id, ts_id, student_user_id):
+    column_names = ['rank_v as student_rank',
+                    'total_students',
+                    "to_char(score,'999.99') as score",
+                    "to_char(total_score,'999.99') as total_score",
+                    "to_char(percentile_score,'999.99') as percentile_score"
+                    ]
+
+    # Combining the two tests
     '''
     sql_stmt = "SELECT " \
                "rank_v as student_rank, " \
