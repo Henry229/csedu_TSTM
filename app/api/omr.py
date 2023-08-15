1577d4c34e917eb04ade1d4707c08afb30f75e3e
tailored_cseducation <itsupport@cseducation.com.au>
2023-08-15 21:05:11 +1000

    test

--- app/api/omr.py
+++ app/api/omr.py
@@ -393,7 +393,9 @@ def omr_writing():
                 '''
                 log.debug("chs3")
                 stage = len(stage_data) + 1
-                log.debug("chs4 $d" % int(branching[end:comma]))
+                #log.debug("chs4 $d" % int(branching[end:comma]))
+                log.debug("chs4 $s" % branching)
+
                 stage_data.append({'stage': stage, 'testlet_id': int(branching[end:comma]), 'percentile': 0})
                 log.debug("chs5")
 
