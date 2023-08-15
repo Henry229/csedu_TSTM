7a9c34022438bc4973ec7cb6062b005893cdf0cb
tailored_cseducation <itsupport@cseducation.com.au>
2023-08-15 16:17:51 +1000

    test

--- app/api/omr.py
+++ app/api/omr.py
@@ -472,7 +472,10 @@ def omr_writing():
                 log.debug("chs7 %s" % questionNo)
                 candidate_mark_detail["%s" % questionNo] = score.get("Answer")
 
-            log.debug("chs8")
+            log.debug("chs8 %s" % marking.id)
+            log.debug("chs8 %s" % marker_branch.marker_id)
+            log.debug("chs8 %s" % candidate_file_link_json)
+            log.debug("chs8 %s" % candidate_mark_detail)
             marking_writing = MarkingForWriting(marking_id=marking.id, marker_id=marker_branch.marker_id)
             marking_writing.candidate_file_link = candidate_file_link_json
             marking_writing.candidate_mark_detail = candidate_mark_detail
